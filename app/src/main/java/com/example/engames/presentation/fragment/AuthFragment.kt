ff599721edaf458089049c13fc296ffbea000615
MikhailAndroidPro <vladosdudos@shisui.ru>
2025-06-05 13:58:46 +0300

    prepare auth fragment

--- app/src/main/java/com/example/engames/presentation/fragment/AuthFragment.kt
+++ app/src/main/java/com/example/engames/presentation/fragment/AuthFragment.kt
@@ -1,28 +1,40 @@
 package com.example.engames.presentation.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.text.method.PasswordTransformationMethod
 import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.engames.R
 import com.example.engames.app.App
 import com.example.engames.databinding.FragmentAuthBinding
 import com.example.engames.presentation.base.fragment.BaseFragment
 
+
 class AuthFragment : BaseFragment<FragmentAuthBinding>(
     FragmentAuthBinding::inflate
 ) {
+    private var isPasswordHidden = true
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         activity().hideNavigationView()
     }
-    private fun authUser(){
-        App.sharedManager.logIn()
-    }
-
     override fun applyClick() {
         super.applyClick()
-
+        binding.eyeBtn.setOnClickListener{
+            if (isPasswordHidden) {
+                binding.eyeBtn.setImageDrawable(context().getDrawable(R.drawable.eye_open))
+                binding.passwordEditText.transformationMethod = null
+            } else {
+                binding.eyeBtn.setImageDrawable(context().getDrawable(R.drawable.eye_close))
+                binding.passwordEditText.transformationMethod = PasswordTransformationMethod()
+            }
+            isPasswordHidden = !isPasswordHidden
+        }
+        binding.signUpText.setOnClickListener{
+            findNavController().navigate(R.id.action_authFragment_to_regFragment)
+        }
+    }
+    private fun authUser(){
+        App.sharedManager.logIn()
     }
 }
