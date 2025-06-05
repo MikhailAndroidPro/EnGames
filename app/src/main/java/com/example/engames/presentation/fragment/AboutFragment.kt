0b4e5f56efbbcf3237514f27617c32177f04f9ac
MikhailAndroidPro <vladosdudos@shisui.ru>
2025-06-05 18:31:05 +0300

    finish about

--- app/src/main/java/com/example/engames/presentation/fragment/AboutFragment.kt
+++ app/src/main/java/com/example/engames/presentation/fragment/AboutFragment.kt
@@ -1,5 +1,7 @@
 package com.example.engames.presentation.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,9 +10,28 @@ import android.view.ViewGroup
 import com.example.engames.R
 import com.example.engames.databinding.FragmentAboutBinding
 import com.example.engames.presentation.base.fragment.BaseFragment
+import androidx.core.net.toUri
 
 class AboutFragment : BaseFragment<FragmentAboutBinding>(
     FragmentAboutBinding::inflate
 ) {
+    override fun applyClick() {
+        super.applyClick()
+        with(binding) {
+            writeUsTxt.setOnClickListener {
+                val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+                    data = StringBuilder(resources.getString(R.string.creator_email)).append(
+                        "?subject=${
+                            resources.getString(R.string.engames_issue)
+                        }"
+                    ).toString().toUri()
+                    putExtra(Intent.EXTRA_SUBJECT, resources.getString(R.string.engames_issue))
+                }
 
+                if (emailIntent.resolveActivity(it.context.packageManager) != null) {
+                    it.context.startActivity(emailIntent)
+                }
+            }
+        }
+    }
 }
