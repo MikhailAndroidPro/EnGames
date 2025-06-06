6de494ed0fbd4f249add85cdbecd451f2f8a5063
MikhailAndroidPro <vladosdudos@shisui.ru>
2025-06-06 16:56:51 +0300

    auth done

--- app/src/main/java/com/example/engames/presentation/fragment/AuthFragment.kt
+++ app/src/main/java/com/example/engames/presentation/fragment/AuthFragment.kt
@@ -3,17 +3,22 @@ package com.example.engames.presentation.fragment
 import android.os.Bundle
 import android.text.method.PasswordTransformationMethod
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.engames.R
 import com.example.engames.app.App
+import com.example.engames.data.ResponseState
 import com.example.engames.databinding.FragmentAuthBinding
+import com.example.engames.presentation.base.BaseViewModel
 import com.example.engames.presentation.base.fragment.BaseFragment
+import com.example.engames.presentation.viewmodel.AuthViewModel
 
 
 class AuthFragment : BaseFragment<FragmentAuthBinding>(
     FragmentAuthBinding::inflate
 ) {
     private var isPasswordHidden = true
+    override val viewModel: AuthViewModel by viewModels()
 
     override fun applyClick() {
         super.applyClick()
@@ -27,7 +32,7 @@ class AuthFragment : BaseFragment<FragmentAuthBinding>(
                     if (isPasswordHidden) PasswordTransformationMethod.getInstance() else null
                 passwordEditText.setSelection(passwordEditText.text.length)
             }
-            signUpText.setOnClickListener{
+            signUpText.setOnClickListener {
                 findNavController().navigate(R.id.action_authFragment_to_regFragment)
             }
             signInButton.setOnClickListener {
@@ -35,8 +40,29 @@ class AuthFragment : BaseFragment<FragmentAuthBinding>(
             }
         }
     }
-    private fun authUser(){
+
+    private fun authUser() {
         if (binding.checkboxRememberMe.isChecked) App.sharedManager.logIn()
-        findNavController().navigate(R.id.action_authFragment_to_gamesFragment)
+        viewModel.loginUser(
+            context(),
+            binding.emailEditText.text.toString(),
+            binding.passwordEditText.text.toString()
+        )
+    }
+
+    override fun setObservers() {
+        super.setObservers()
+        viewModel.state.observe(viewLifecycleOwner) {
+            when (it) {
+                is ResponseState.Success -> {
+                    showToast(resources.getString(R.string.successfully_authorized))
+                    findNavController ().navigate(R.id.action_authFragment_to_gamesFragment)
+                }
+
+                is ResponseState.Error -> {
+                    showToast(it.message)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/engames/presentation/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/example/engames/presentation/viewmodel/AuthViewModel.kt
@@ -1,9 +1,37 @@
 package com.example.engames.presentation.viewmodel
 
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.engames.R
+import com.example.engames.app.App
+import com.example.engames.data.ResponseState
 import com.example.engames.presentation.base.BaseViewModel
+import kotlinx.coroutines.launch
 
 class AuthViewModel : BaseViewModel() {
+    private val _state = MutableLiveData<ResponseState<Unit>>()
+    val state: LiveData<ResponseState<Unit>> = _state
+
+    fun loginUser(context: Context, email: String, password: String) {
+        viewModelScope.launch {
+            try {
+                val request = App.authRepository.loginUser(context, email, password)
+                if (request is ResponseState.Success) {
+                    _state.value = ResponseState.Success(Unit)
+                } else {
+                    _state.value = ResponseState.Error(
+                        context.resources.getText(R.string.user_not_found).toString()
+                    )
+                }
+            } catch (e: Exception) {
+                _state.value = ResponseState.Error(e.message.toString())
+            }
+        }
+    }
+
     override fun resumeState() {
 
     }
